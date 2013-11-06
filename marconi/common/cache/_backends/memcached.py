6b59f146438f232101c359b040a6293228085bf8
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-11-06 12:14:28 -0500

    fix(caching): memcached backend disobeys contract
    Updated the oslo.cache memcached backend so that set returns
    True/False. To do so, it was a matter of checking that memcached
    client's return value was nonzero, as per the docs.
    
    Change-Id: I5622fbab1b08eca7dd1ee01e053cb1b9fc860a6e

--- marconi/common/cache/_backends/memcached.py
+++ marconi/common/cache/_backends/memcached.py
@@ -57,7 +57,7 @@ class MemcachedBackend(backends.BaseCache):
 
     def set(self, key, value, ttl=0):
         key = self._prepare_key(key)
-        self._cache.set(key, value, self._get_ttl(ttl))
+        return self._cache.set(key, value, self._get_ttl(ttl)) != 0
 
     def unset(self, key):
         self._cache.delete(self._prepare_key(key))
